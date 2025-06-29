f3752d71f5ab45a5f30a39ae4cdb83e0182f3ce7
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-06-29 11:19:25 -0500

    :fire: configuracion para render

--- app/services/firebase.py
+++ app/services/firebase.py
@@ -2,14 +2,23 @@ import firebase_admin
 from firebase_admin import credentials, storage, firestore
 import os
 from dotenv import load_dotenv
+import json 
 
-# Cargar variables de entorno
-load_dotenv()
+# Configuración para Render
+if "RENDER" in os.environ:  # Verifica si está en producción
+    firebase_config = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(firebase_config)
+    bucket_name = os.environ["FIREBASE_STORAGE_BUCKET"]
+else:  # Desarrollo local
+    from dotenv import load_dotenv
+    load_dotenv()
+    cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
+    bucket_name = os.getenv("FIREBASE_STORAGE_BUCKET")
 
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
 firebase_admin.initialize_app(cred, {
-    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET")
+    "storageBucket": bucket_name
 })
+
 
 db = firestore.client()
 bucket = storage.bucket()
