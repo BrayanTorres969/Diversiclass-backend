e9821b245203da6998d2f74215d006c9d0a4db39
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-06-29 12:27:55 -0500

    :fire: cambio a punto correcto

--- app/services/firebase.py
+++ app/services/firebase.py
@@ -2,21 +2,13 @@ import firebase_admin
 from firebase_admin import credentials, storage, firestore
 import os
 from dotenv import load_dotenv
-import json 
 
-# Configuración para Render
-if "RENDER" in os.environ:  # Verifica si está en producción
-    firebase_config = json.loads(os.environ["FIREBASE_CREDENTIALS"])
-    cred = credentials.Certificate(firebase_config)
-    bucket_name = os.environ["FIREBASE_STORAGE_BUCKET"]
-else:  # Desarrollo local
-    from dotenv import load_dotenv
-    load_dotenv()
-    cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
-    bucket_name = os.getenv("FIREBASE_STORAGE_BUCKET")
+# Cargar variables de entorno
+load_dotenv()
 
+cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
 firebase_admin.initialize_app(cred, {
-    "storageBucket": bucket_name
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET")
 })
 
 
